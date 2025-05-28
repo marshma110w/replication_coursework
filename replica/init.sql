889d9279e99e39d30a3b8cf2c9d287de53dd97bc
Michael <me@chichunov.ru>
2025-05-28 16:05:07 +0300

    add shipments table

--- replica/init.sql
+++ replica/init.sql
@@ -24,6 +24,7 @@ CREATE TABLE items (
     barcode VARCHAR(100) UNIQUE,
     category_id INTEGER REFERENCES Item_categories(id),
     weight DECIMAL(10, 2),
+    quantity INTEGER NOT NULL DEFAULT 1,
     warehouse_id INTEGER REFERENCES Warehouses(id)
 );
 
@@ -38,6 +39,29 @@ CREATE TABLE employees (
     warehouse_id INTEGER REFERENCES Warehouses(id)
 );
 
+CREATE TYPE shipment_status AS ENUM ('PENDING', 'PREPARED', 'COMPLETED');
+
+-- Основная таблица выдачи
+CREATE TABLE shipments (
+    id SERIAL PRIMARY KEY,
+    warehouse_id INTEGER NOT NULL REFERENCES warehouses(id),
+    courier_id INTEGER REFERENCES employees(id), -- теперь может быть NULL
+    status shipment_status NOT NULL DEFAULT 'PENDING',
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    completed_at TIMESTAMP
+);
+
+-- Таблица состава выдачи (товары и их количество)
+CREATE TABLE shipment_items (
+    shipment_id INTEGER NOT NULL REFERENCES shipments(id),
+    item_id INTEGER NOT NULL REFERENCES items(id),
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    PRIMARY KEY (shipment_id, item_id)
+);
+
+CREATE INDEX idx_shipments_status ON shipments(status);
+CREATE INDEX idx_shipments_warehouse ON shipments(warehouse_id);
+CREATE INDEX idx_shipments_courier ON shipments(courier_id);
 
 -- Создание пользователя для репликации
 CREATE USER repl_user WITH REPLICATION LOGIN PASSWORD 'repl_password';
